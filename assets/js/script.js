9f16dd762f839faae68295ed326d60ee05650d3a
Suejin Kim <kim.suejin.20@gmail.com>
2021-03-01 22:01:20 -0500

    questions populate and event listeners attached to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,258 @@
-//Quiz question elements
-var questionEL = document.getElementById("question-box")
+// Series of questions stored in an object. The answers and the questions will be accessable in that object.
 var questions = [
     {
-        prompt: "Common data types DO NOT include:",
-        choices: ["boolean", "array", "taxes", "string"],
-        answer: 2 //index number of correct answer
+      prompt: "Common data types DO NOT include:",
+      choices: ["boolean", "string", "taxes", "number"],
+      answer: "taxes",
     },
     {
-        prompt: "Classes are denoted by a ______ selector in CSS.",
-        choices: ["asterisk *", "hash #", "period .", "backslash /"],
-        answer: 2 //index number of correct answer
+      prompt: "Classes are denoted by a ______ selector in CSS.",
+      choices: ["asterisk", "hash", "period", "backslash"],
+      answer: "period",
     },
     {
-        prompt: "HTML tags are enclosed in ______.",
-        choices: ["square brackets []", "angle brackets <>", "curly braces {}", "parentheses ()"],
-        answer: 1 //index number of correct answer
+      prompt: "HTML tags are enclosed in ______.",
+      choices: ["square brackets", "angle brackets", "curly braces", "parentheses"],
+      answer: "angle brackets",
     },
-]
-var correctAnswer = "";
+    {
+      prompt: "_________ is a series of statements that have been grouped together to perform a specific task. ",
+      choices: ["An array", "A DOM element", "CSS", "A function"],
+      answer: "A function",
+    },
+    {
+      prompt: "All of the following are third-party-APIs EXCEPT:",
+      choices: ["Java", "jQuery", "Bootstrap", "Google Fonts"],
+      answer: "Java",
+    }
+];
+var score = 0;  // a variable to accumulate correct answers 
+var questionsIndex = 0;  // keeping track of which question we're on
+//var correctChoice = "";
+var choiceAEl = "";
+var choiceBEl = "";
+var choiceCEl = "";
+var choiceDEl = "";
+
 
 //Display Pages
-var startPageEl = document.getElementById("landing-page")
-var questionPageEl = document.getElementById("question-page")
-var finalScorePageEl = document.getElementById("final-score-page")
-var buttonDivEl = document.getElementsByClassName("button-div")
+var landingDiv = document.getElementById("landing-page");
+var questionDiv = document.getElementById("question-page");
+var promptDiv = document.getElementById("prompt");
+var resultsDiv = document.getElementById("results-page");
+var buttonDiv = $('#button-div');
+var feedbackDiv = document.getElementById("feedback");
+var startBtnEl = "";
+var scoresDiv = document.getElementById("high-scores-div");
 
-//Button Elements
-var startBtnEl = document.getElementById("startBtn")
-var page = 0;
-var submitBtnEl = document.getElementById("submitBtn")
+//Timer Variables
+var secondsLeft = 100; // start timer at 100 seconds
+var timeEl = document.getElementById("time"); 
 
-//Timer
-var timeEl = document.querySelector(".time");
-var secondsLeft = 100;
+var scoresDiv = document.getElementById("scores-div"); //div for high scores
+var viewScoresBtn = document.getElementById("view-scores");  //button for high scores
+//var startBtnEl = document.getElementById("startBtn");  //start button div
+
+var emptyArray = [];  // an array to store high scores
+var storedArray = JSON.parse(window.localStorage.getItem("highScores"));  // the array of high scores from local storage
+
+function init() {
+  startBtnEl = document.createElement("button");
+  startBtnEl.innerText = "Start Quiz";
+  buttonDiv.append(startBtnEl);
+};
+
 
 function setTime() {
-    var timerInterval = setInterval
-    (
-        function() {
+  displayQuestions();
+    var timerInterval = setInterval(function() {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left";
-  
-        if(secondsLeft === 0) {
+        timeEl.innerText = secondsLeft + " seconds left";
+        if(secondsLeft == 0) {
             clearInterval(timerInterval);
-            sendMessage();
-      }
-    }, 1000
-
-    )
-    ;
-  }
-  
-
-function sendMessage(){
-    console.log("your time is up!")
-}
-
-//Quiz begins
-function init() {
-    startPageEl.style.display="block";
-    questionPageEl.style.display="none";
-    finalScorePageEl.style.display="none";
-}
+            displayScore();
+      };
+    }, 1000);
+  };
 
 function displayQuestions() {
-    setTime();
-    var questionPromptEl = document.getElementById("question");
-    var choiceAEl = document.getElementById("choice-a");
-    var choiceBEl = document.getElementById("choice-b");
-    var choiceCEl = document.getElementById("choice-c");
-    var choiceDEl = document.getElementById("choice-d");
-    var questionsIndex = 0
+  startBtnEl.remove();
+  promptDiv.innerHTML = "";
+  buttonDiv.empty();
 
-    questionPageEl.style.display="block";
-    startPageEl.style.display="none";
+  var choiceAEl = document.createElement("button");
+  var choiceBEl = document.createElement("button");
+  var choiceCEl = document.createElement("button");
+  var choiceDEl = document.createElement("button");
 
-    switch (questionsIndex) {
-        case 0:
-        questionPromptEl.textContent = questions[0].prompt;
-        choiceAEl.textContent = questions[0].choices[0];
-        choiceBEl.textContent = questions[0].choices[1];
-        choiceCEl.textContent = questions[0].choices[2];
-        choiceDEl.textContent = questions[0].choices[3];
-        
-        correctChoice = questions[0].answer;
-        questionsIndex = questionsIndex + 1;
+  choiceAEl.setAttribute('class', 'choice-btn');
+  choiceBEl.setAttribute('class', 'choice-btn');
+  choiceCEl.setAttribute('class', 'choice-btn');
+  choiceDEl.setAttribute('class', 'choice-btn');
 
-        break;
+  choiceAEl.addEventListener('click', checkAnswer);
+  choiceBEl.addEventListener('click', checkAnswer);
+  choiceCEl.addEventListener('click', checkAnswer);
+  choiceDEl.addEventListener('click', checkAnswer);
 
-        case 1:
-        questionPromptEl.textContent = questions[1].prompt;
+  buttonDiv.append(choiceAEl);
+  buttonDiv.append(choiceBEl);
+  buttonDiv.append(choiceCEl);
+  buttonDiv.append(choiceDEl);
+
+  switch (questionsIndex) {
+      case 0:
+      promptDiv.textContent = questions[0].prompt;
+      choiceAEl.textContent = questions[0].choices[0];
+      choiceBEl.textContent = questions[0].choices[1];
+      choiceCEl.textContent = questions[0].choices[2];
+      choiceDEl.textContent = questions[0].choices[3];
+      
+      correctChoice = questions[0].answer;
+      break;
+
+      case 1:
+        promptDiv.textContent = questions[1].prompt;
         choiceAEl.textContent = questions[1].choices[0];
         choiceBEl.textContent = questions[1].choices[1];
         choiceCEl.textContent = questions[1].choices[2];
         choiceDEl.textContent = questions[1].choices[3];
 
         correctChoice = questions[1].answer;
-        questionsIndex = questionsIndex + 1;
+      break;
 
-        break;
-
-        case 2:
-        questionPromptEl.textContent = questions[2].prompt;
+      case 2:
+        promptDiv.textContent = questions[2].prompt;
         choiceAEl.textContent = questions[2].choices[0];
         choiceBEl.textContent = questions[2].choices[1];
         choiceCEl.textContent = questions[2].choices[2];
         choiceDEl.textContent = questions[2].choices[3];
 
         correctChoice = questions[2].answer;
-        questionsIndex = questionsIndex + 1;
+      break;
 
-        case 3:
-        questionPromptEl.textContent = questions[3].prompt;
+      case 3:
+        promptDiv.textContent = questions[3].prompt;
         choiceAEl.textContent = questions[3].choices[0];
         choiceBEl.textContent = questions[3].choices[1];
         choiceCEl.textContent = questions[3].choices[2];
         choiceDEl.textContent = questions[3].choices[3];
 
         correctChoice = questions[3].answer;
-        questionsIndex = questionsIndex + 1;
-        break;
+      break;
+
+      case 4:
+        promptDiv.textContent = questions[4].prompt;
+        choiceAEl.textContent = questions[4].choices[0];
+        choiceBEl.textContent = questions[4].choices[1];
+        choiceCEl.textContent = questions[4].choices[2];
+        choiceDEl.textContent = questions[4].choices[3];
+    
+        correctChoice = questions[4].answer;
+      break;
+
+      case 5:
+        displayScore();
+      break;
+  }
+
+};
+
+function checkAnswer() {
+    if (this.innerHTML == correctChoice) {
+       score += 10;
+      } else {
+        secondsLeft -= 10;
+    };
+    questionsIndex ++;
+    displayQuestions();
+}
+
+if (questionsIndex == questions.length) {
+  displayScore();
+}
+
+function displayScore() {
+    timeEl.remove();
+    //choices.textContent = "";
+  buttonDiv.empty();
+
+    var userInitials = document.createElement('input');
+    var submitBtn = document.createElement('button');
+
+    resultsDiv.innerHTML = "Your score = " + score + ".    Initials: "
+    userInitials.setAttribute("type", "text");
+    submitBtn.innerText = "Submit";
+  
+    submitBtn.addEventListener('click', function(event) {
+      event.preventDefault();
+      var scoresArray = defineScoresArray(storedArray, emptyArray);
+
+      var initials = userInitials.value;
+      var userAndScore = {
+        initials: initials,
+        score: score,
+      };
+  
+      scoresArray.push(userAndScore);
+
+      window.localStorage.setItem("highScores", JSON.stringify(scoresArray));
+
+      displayAllScores();
+
+      submitBtn.remove();
+  
     }
+    );
+
+    resultsDiv.append(userInitials);
+    resultsDiv.append(submitBtn);
+
+};
+function defineScoresArray(arr1, arr2) {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+};
+
+function displayAllScores() {
+  timeEl.remove();
+  //startBtnEl.remove();
+  resultsDiv.remove();
+let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+scoresArray.forEach(obj => {
+  let initials = obj.initials;
+  let storedScore = obj.score;
+  let resultsP = document.createElement("p");
+  resultsP.innerText = `${initials}: ${storedScore}`;
+  scoresDiv.append(resultsP);
+});
 }
 
 
-var score = "" //score will be (correct question total * 10) + time left
-function gotRight() {
-    //when the player clicks the button with the correct answer, the computer will increase the score by 10
-}
+init();
+startBtnEl.addEventListener("click", setTime);
+//displayScore();
 
+//Function to display the questions
+  // with a parameter of the question to be asked.
+  // loop through the answers and apend buttons for each on the page
+  //dynamically updated/overwrite the div with questions and answers
 
-startBtnEl.addEventListener("click", displayQuestions)
-init()
+//on click function that checks if the answer is correct
+  // conditional statement.  
+  // If the answer is incorrect decrement 15 seconds from the countdown then move to the next question
 
+// If the answer is correct just move to the next question
 
-//need to store questions in the local storage?
-//localStorage.setItem("quiz-questions", JSON.stringify(questions));
+//A set interval function
+  // decrements time at a set interval
+  // udates the display
 
-//then get items to populate 
-//var lastQuestion = JSON.parse(localStorage.getItem("quiz-questions"));
+//HTML docment that includes a div or secion where the questions will be displayed
 
+//capture the users initials at the end with their score for display at the end of the quiz and store in localStorage.
+  //use a global variable to accumulate correct answers then save then use on click to save to localStorage
